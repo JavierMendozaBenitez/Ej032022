70a01ff549d4b1cc86d1df6c7d6a55f66fe5bc12
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2022-04-04 17:02:05 -0300

    04-04-2022

--- Ej I02 Des cont/Biblioteca/Validador.cs
+++ Ej I02 Des cont/Biblioteca/Validador.cs
@@ -2,7 +2,19 @@
 
 namespace Biblioteca
 {
-    public class Class1
+    public class Validador
     {
+        public static bool ValidarRespuesta(string respuesta, string letraCorrecta)
+        {
+            bool retorno = false;
+
+            if(respuesta == letraCorrecta)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+
     }
 }
--- Ej I02 Des cont/Ej I02 Des cont/Program.cs
+++ Ej I02 Des cont/Ej I02 Des cont/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using Biblioteca;
+
+/*Realizar un programa que sume números enteros hasta que el usuario lo determine por medio de un mensaje "¿Desea continuar? (S/N)".
+
+Crear la clase Validador y el método estático ValidarRespuesta, que validará el ingreso de respuestas.
+
+El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.*/
 
 namespace Ej_I02_Des_cont
 {
@@ -6,7 +13,27 @@ namespace Ej_I02_Des_cont
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroIngresado;
+            string letraIngresada;
+            int acumulador = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero");
+
+                while (!int.TryParse(Console.ReadLine(), out numeroIngresado))
+                {
+                    Console.WriteLine("Error. Reingrese el numero");
+                }
+
+                acumulador += numeroIngresado;
+
+                Console.WriteLine("¿Desea continuar? (S/N)");
+                letraIngresada = Console.ReadLine();
+
+            } while (Validador.ValidarRespuesta(letraIngresada, "S"));
+
+            Console.WriteLine($"Numero acumulado {acumulador}");
         }
     }
 }
