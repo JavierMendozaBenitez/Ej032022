ec7ff83de720369f9cb7513ad7f7a425043056df
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2022-04-12 14:37:47 -0300

    12-04-2022 segundo

---
+++ Ej I01 Sumador/Biblioteca/Sumador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+        public Sumador() : this(0)
+        {}
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas += 1;
+
+            return a + b;
+        }
+
+        public string Sumar (string a, string b)
+        {
+            this.cantidadSumas += 1;
+
+            return string.Format($"{a}{b}");
+        }
+
+        public static explicit operator int(Sumador sumador)
+        {
+            return sumador.cantidadSumas;
+        }
+
+        public static long operator +(Sumador a, Sumador b)
+        {
+            return (int)a.cantidadSumas + (int)b.cantidadSumas;
+        }
+
+        public static bool operator | (Sumador a, Sumador b)
+        {
+            bool retorno = false;
+
+            if (a.cantidadSumas == b.cantidadSumas)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        
+    }
+}
---
+++ Ej I01 Sumador/Ej I01 Sumador/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Biblioteca;
+
+namespace Ej_I01_Sumador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long a = 10;
+            long b = 55;
+            string uno = "Uno";
+            string dos = "Dos";
+
+            long c = 100;
+            long d = 550;
+            string tres = "Tres";
+            string cuatro = "Cuatro";
+
+            long e = 200;
+            long f = 750;
+            string cinco = "Cinco";
+            string seis = "Seis";
+
+            Sumador sumador1 = new Sumador();
+
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador1: {0}", ((int)sumador1));
+
+
+            Console.WriteLine("Muestro la suma de enteros de sumador1: {0}", sumador1.Sumar(a, b));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador1: {0}", ((int)sumador1));
+
+
+            Console.WriteLine("Muestro la suma de string de sumador1: {0}", sumador1.Sumar(uno, dos));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador1: {0}", ((int)sumador1));
+
+
+
+
+            Sumador sumador2 = new Sumador();
+
+            Console.WriteLine("\n\nResultado de la cantidad de sumas de sumador2: {0}", ((int)sumador2));
+
+
+            Console.WriteLine("Muestro la suma de enteros de sumador2: {0}", sumador2.Sumar(c, d));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador2: {0}", ((int)sumador2));
+
+ 
+            Console.WriteLine("Muestro la suma de string de sumador2: {0}", sumador2.Sumar(tres, cuatro));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador2: {0}", ((int)sumador2));
+
+
+
+            //Sobrecargas
+
+            Console.WriteLine("\nMuestro la sobrecarga del +, cuantas sumas tienen en total: {0}", sumador1 + sumador2);
+            Console.WriteLine("Muestro la sobrecarga del |, muestros si tienen la misma cantidad de sumas: {0}", sumador1 | sumador2);
+
+
+            Console.WriteLine("\nMuestro la suma de enteros de sumador2: {0}", sumador2.Sumar(e, f));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador2: {0}", ((int)sumador2));
+            Console.WriteLine("Muestro la suma de string de sumador2: {0}", sumador2.Sumar(cinco, seis));
+            Console.WriteLine("Resultado de la cantidad de sumas de sumador2: {0}", ((int)sumador2));
+
+            Console.WriteLine("\nMuestro la sobrecarga del +, cuantas sumas tienen en total: {0}", sumador1 + sumador2);
+            Console.WriteLine("Muestro la sobrecarga del |, muestros si tienen la misma cantidad de sumas: {0}", sumador1 | sumador2);
+
+        }
+    }
+}
