a362d2f9428480fe3d30a092e00a4c027cbc9055
JavierMendozaBenitez <javier.mendoza.benitez@gmail.com>
2022-05-05 18:59:26 -0300

    05-05-2022 1era

---
+++ Ej C01 Herencia dep/Entidades/DirectorTecnico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DirectorTecnico: Persona
+    {
+        private DateTime fechaNacimiento;
+
+        private DirectorTecnico(string nombre)
+            :base(nombre)
+        {            
+        }
+
+        public DirectorTecnico(string nombre, DateTime fechaNacimiento)
+            : this(nombre)
+        {
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get
+            {
+                return this.fechaNacimiento;
+            }
+        }
+        public new string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine("Fecha de nacimiento: " + this.FechaNacimiento);
+
+            return sb.ToString();
+        }
+
+        public static bool operator == (DirectorTecnico dt1, DirectorTecnico dt2)
+        {
+            return string.Compare(dt1.Nombre, dt2.Nombre) == 0 && DateTime.Compare(dt1.FechaNacimiento, dt2.FechaNacimiento) == 0;
+        }
+
+        public static bool operator !=(DirectorTecnico dt1, DirectorTecnico dt2)
+        {
+            return !(dt1 == dt2);
+        }
+    }
+}
---
+++ Ej C01 Herencia dep/Entidades/Jugador.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Jugador: Persona
+    {
+        private int partidosJugados;
+        private int totalGoles;
+                
+        public Jugador(int dni, string nombre)
+            : base(dni, nombre)
+        {
+        }
+        public Jugador(int dni, string nombre, int totalGoles, int totalPartidos)
+            : this(dni, nombre)
+        {
+            this.TotalGoles = totalGoles;
+            this.PartidosJugados = totalPartidos;
+        }
+
+        //propiedades
+            
+            //solo lectura
+
+        public int PartidosJugados
+        {
+            get
+            {
+                return this.partidosJugados;
+            }
+            set
+            {
+                this.partidosJugados = value;
+            }
+        }
+
+
+        public int TotalGoles
+        {
+            get { return this.totalGoles; }
+            set
+            {
+                this.totalGoles = value;
+            }
+        }
+
+        public float PromedioGoles
+        {
+            get { return (float)this.totalGoles / this.partidosJugados; }
+        }
+
+              
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.MostrarDatos());
+            sb.AppendLine("Partidos jugados: " + this.PartidosJugados);
+            sb.AppendLine("Promedio de Goles: " + this.PromedioGoles.ToString());
+            sb.AppendLine("Total de Goles: " + this.TotalGoles);
+
+            return sb.ToString();
+        }
+
+        public static bool operator == (Jugador j1, Jugador j2)
+        {
+            return j1.Dni == j2.Dni;
+        }
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1.Dni == j2.Dni);
+        }
+    }
+}
